761e7f4738ccb5e90339dc47eb36bffd5171f80a
kkking789 <140388891+kkking789@users.noreply.github.com>
2024-01-30 21:05:58 +0800

    Update main.py
    添加了网络连接错误的判断，防止下载中途因为网络波动被打断
    现在下载的是详细图即原图

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 """
-Version: 1.0:
 这是一个用来爬bing图片做分类数据集的简易爬虫
 
 可以自定义爬的图片的数量，以及爬的种类
 每次下载图片的time.sleep最好不要低于0.3，不然有被封IP的风险，可以把等待时间调小点，这样下的快
-headers可以在浏览器的开发者模式下找到，具体从网上查怎么找
+headers最好改成自己的，现在这份代码里的是我的headers，这玩意可以在浏览器的开发者模式下找到，具体从网上查怎么找
 另外引入了一个额外的库PIL，下载方式直接命令窗输入 pip install Pillow 即可
 
 这份爬虫对下载的图片的宽长积存在一定限制，这样防止下载到奇奇怪怪的图标，来污染数据集
@@ -15,23 +14,29 @@ python main.py --你想要修改的参量1=你想要把它改成的值1 --你想
 
 另外，为什么整个世界上有FPGA这么恶心的东西啊，服了
 
-Version: 1.1:
-添加了自动创建数据集的文件夹
-在下载图片时随机抽取10张图片，5张放进测试集，5张放入验证集，其余放入训练集
+Author: kkking
+Date: 23.11.27
+Version: 1.0
 
-Author: kkking789
-Date: 23.11.28
-Version: 1.1
+Author: kkking
+Date: 24.1.30
+Version: 1.2
+
 """
-
+import PIL
 import requests
 import re
 import os
 import time
 import argparse
 import random
+
+import urllib3
 from PIL import Image
 from urllib import parse
+from urllib.parse import unquote
+from requests.adapters import HTTPAdapter
+
 
 
 def print_progress_bar(current_value, total_value, bar_length=50):
@@ -57,14 +62,25 @@ class SpiderClass:
         self.html = None
 
     def find_src_strings(self):  # 获取返回的图片的src
-        pattern = re.compile(r'src="(.*?)"')
+        pattern = re.compile(r'mediaurl=(.*?)&amp;exph=')
         matches = re.findall(pattern, self.html)
+        matches = matches[::3]
         return matches
 
     def get_img_link(self):  # 获取网页的feedback的图片的链接
         searchName_parse = parse.quote(self.search)
         self.urlSearch = self.urlEmpty.format(searchName_parse)
-        strhtml = requests.get(self.urlSearch, headers=self.headers)
+        s = requests.session()
+        s.keep_alive = False
+        s.mount('http://', HTTPAdapter(max_retries=3))
+        s.mount('https://', HTTPAdapter(max_retries=3))
+        try:
+            strhtml = s.get(self.urlSearch, headers=self.headers, timeout=10)
+        except requests.exceptions.ConnectionError:
+            return None
+        except requests.exceptions.ChunkedEncodingError:
+            return None
+            print("网络错误")
         self.html = strhtml.text
         img_link = self.find_src_strings()
         return img_link
@@ -87,7 +103,18 @@ class SpiderClass:
         test_numbers = random_numbers[0:4]
         valid_numbers = random_numbers[5:9]
         for img_link in img_links:
-            res = requests.get(img_link, headers=self.headers)
+            img_link = unquote(img_link)
+            s = requests.session()
+            s.keep_alive = False
+            s.mount('http://', HTTPAdapter(max_retries=3))
+            s.mount('https://', HTTPAdapter(max_retries=3))
+            try:
+                res = s.get(img_link, headers=self.headers, timeout=10)
+            except requests.exceptions.ConnectionError :
+                res.status_code = 404
+            except requests.exceptions.ChunkedEncodingError:
+                res.status_code = 404
+                print("网络错误")
             if res.status_code == 404:
                 print(f"图片{img_link}下载出错")
             else:
@@ -101,28 +128,38 @@ class SpiderClass:
                 with open(filename, "wb") as f:
                     f.write(res.content)
                     f.close()
-                    img = Image.open(filename)
-                    width, height = img.size
-                    img.close()
-                    if width * height < 2500:  # 宽高积低于2500则视为垃圾图片，将其删去
-                        print("图片长宽积过低，已删去")
+                    try:
+                        img = Image.open(filename)
+                        width, height = img.size
+                        img.close()
+                        if width * height < 2500:  # 宽高积低于2500则视为垃圾图片，将其删去
+                            print("图片长宽积过低，已删去")
+                            os.remove(filename)
+                    except PIL.UnidentifiedImageError as e:
+                        print("图片异常，已删去")
                         os.remove(filename)
                     print_progress_bar((num - 1) * 30 + cnt, self.getNum * 30)
-                    time.sleep(0.5)  # 每次休眠一段时间，防止封IP
+                    time.sleep(0.4)  # 每次休眠一段时间，防止封IP
                 if cnt == 30:
                     return
+            res.close()
+
+def url_change(url, cnt):
+    index = url.find("first=")
+    start_str = "first=" + str(cnt)
+    return url[:index]+start_str+url[len("first=" + str(cnt-1))+index:]
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--search', type=str, default='梅花')
+    parser.add_argument('--search', type=str, default='water')
     parser.add_argument('--headers', type=str, default='')
     parser.add_argument('--savePath', type=str, default='./')
     parser.add_argument('--url', type=str, default='https://cn.bing.com/images/async?q={'
-                                                   '}&first=0&count=29&cw=1280&ch=780&relp=32&apc=0&datsrc=I&layout'
+                                                   '}&first=5&count=29&cw=1280&ch=780&relp=32&apc=0&datsrc=I&layout'
                                                    '=RowBased_Landscape&mmasync=1&dgState=x*0_y*0_h*0_c*4_i*126_r*31'
                                                    '&IG=FA7FC853DC934344BA0C794E0E318FAB&SFX=5&iid=images.5571')
-    parser.add_argument('--getNum', type=int, default=1)
+    parser.add_argument('--getNum', type=int, default=4)
     return parser.parse_known_args()[0]
 
 
@@ -130,6 +167,7 @@ def main(opt):
     Spider = SpiderClass(opt)
     Spider.make_output_dir()
     for currentNum in range(1, opt.getNum + 1):
+        Spider.urlEmpty = url_change(Spider.urlEmpty, currentNum)
         img_links = Spider.get_img_link()
         Spider.get_img(img_links, currentNum)
     print("批量图片下载完成")
